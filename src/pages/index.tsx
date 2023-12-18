76119b0c82497292224386db9ea4d732add2cd39
Matheus Santana <santalvesmatheus@gmail.com>
2023-12-18 14:56:42 -0300

    update image tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <main className="hero min-h-screen font-medium">
         <div className="hero-content text-center">
           <div className="max-w-md flex flex-col justify-center">
-            <Image src={'https://i.postimg.cc/cJnKqhgV/logo-ismafer.jpg'} alt='logo' width={200} className="self-center" />
+            <img src={'https://i.postimg.cc/cJnKqhgV/logo-ismafer.jpg'} alt='logo' className="self-center" />
             <p className="py-6 font-light text-[--light]">Ferramentas de uso rápido para criação de anúncios da Ismafer.</p>
             <div className="flex flex-col gap-4">
               <Link href={'/tools/cutter'}>
