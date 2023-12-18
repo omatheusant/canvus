a093828a0e47256703cfeee02b4bfe57794aa655
Matheus Santana <santalvesmatheus@gmail.com>
2023-12-18 12:36:00 -0300

    update header

--- src/components/Editor/Header.jsx
+++ src/components/Editor/Header.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { CiSaveDown2 } from "react-icons/ci";
 
 export const Header = ({ canvas }) => {
@@ -7,14 +8,16 @@ export const Header = ({ canvas }) => {
     link.href = canvas.toDataURL('image/png', 1.0);
     link.click();
   };
- 
+
   return (
     <div className='bg-[--bg-color] shadow-md shadow-black w-full h-[14vh] flex items-center z-20 px-5 justify-between fixed top-0'>
       <div className='w-[25vh]'>
-        <img src="https://i.postimg.cc/cJnKqhgV/logo-ismafer.jpg" alt="logo" />
+        <Link href={'/'}>
+          <img src="https://i.postimg.cc/cJnKqhgV/logo-ismafer.jpg" alt="logo" />
+        </Link>
       </div>
-      <button className='w-[10vw] h-[3vw] bg-[--brand] font-bold text-[1.4vw] gap-2 flex items-center bg-center py-2 px-5 justify-center text-[--dark]' onClick={handleDownload}> Salvar <CiSaveDown2 className='text-[2vw]'/> </button>
+      <button className='w-[10vw] h-[3vw] bg-[--brand] font-bold text-[1.4vw] gap-2 flex items-center bg-center py-2 px-5 justify-center text-[--dark]' onClick={handleDownload}> Salvar <CiSaveDown2 className='text-[2vw]' /> </button>
     </div>
   );
- }
+}
 
