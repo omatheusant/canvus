74e81bb7d854dd66ab0434655c86f08b327511c0
Matheus <mat.alvessantana@gmail.com>
2023-12-30 09:32:47 -0300

    add psd interpreter

--- src/components/Editor/tools/AddImage.jsx
+++ src/components/Editor/tools/AddImage.jsx
@@ -1,29 +1,50 @@
+import axios from 'axios';
 import { fabric } from 'fabric'
 import { FaCloudUploadAlt } from 'react-icons/fa';
 
 export const AddImage = ({ canvas }) => {
-  const handleImage = (e) => {
+  const handleImage = async (e) => {
     const file = e.target.files[0];
     if (file) {
-      fabric.Image.fromURL(URL.createObjectURL(file), img => {
-        img.set({
-          id: img.id + '_' + Date.now(),
-          angle: 0,
-          padding: 10,
-          selectable: true,
-          cornerColor: 'rgba(255, 255, 255, 0.7)',
-          cornerStrokeColor: 'rgb(0, 0, 0)',
-          transparentCorners: false,
-          borderColor: 'rgb(43, 0, 78)',
-          cornerStyle: 'circle',
-          cornerSize: 40
+      if(file.name.endsWith('.psd')) {
+        console.log('PSD detected!')
+        console.log(file)
+        try {
+          console.log('sending file to server...')
+          const formData = new FormData();
+          formData.append('file', file);
+
+          const response = await axios.post('/api/upload/psd', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          });
+          console.log('PSD uploaded successfully:', response.data);
+        } catch (error) {
+          console.error('Error uploading PSD:', error);
+        }
+      } else {
+        console.log('Non psd file detected!')
+        fabric.Image.fromURL(URL.createObjectURL(file), img => {
+          img.set({
+            id: img.id + '_' + Date.now(),
+            angle: 0,
+            padding: 10,
+            selectable: true,
+            cornerColor: 'rgba(255, 255, 255, 0.7)',
+            cornerStrokeColor: 'rgb(0, 0, 0)',
+            transparentCorners: false,
+            borderColor: 'rgb(43, 0, 78)',
+            cornerStyle: 'circle',
+            cornerSize: 40
+          });
+          canvas.centerObject(img);
+          canvas.add(img);
+          canvas.setActiveObject(img);
+          canvas.renderAll();
+          e.target.value = '';
         });
-        canvas.centerObject(img);
-        canvas.add(img);
-        canvas.setActiveObject(img);
-        canvas.renderAll();
-        e.target.value = '';
-      });
+      }
     }
    }
 
---
+++ src/pages/api/upload/psd/index.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { readPsd, Layer } from 'ag-psd';
+import {fabric} from 'fabric'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method === 'POST') {
+      const fileBuffer: Buffer = req.body;
+      console.log('Received PSD file:', fileBuffer);
+
+      // Use a função readPsd para processar o arquivo PSD
+      const psd = readPsd(fileBuffer);
+
+      // Função para converter camadas em imagens
+      const convertLayersToImages = (layers: Layer[]): fabric.Image[] => {
+        return layers.map((layer, i) => {
+          const image = new fabric.Image(layer.canvas as any, {
+            name: layer.name || `layer-${i}`,
+            left: 0,
+            top: 0,
+            selectable: true,
+          });
+          return image;
+        });
+      };
+
+      // Converter camadas para imagens
+      const images = convertLayersToImages(psd.children || []);
+
+      // Exemplo: Enviar imagens como resposta
+      res.status(200).send({images});
+    } else {
+      // Método HTTP não permitido
+      res.status(405).json({ error: 'Method Not Allowed' });
+    }
+  } catch (error) {
+    console.error('Error processing PSD:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
